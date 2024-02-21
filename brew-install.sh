6c92be8975720cc637129f64d858db5f00eceb4b
twitch.liu <twitch.liu@huolala.cn>
2024-02-21 11:19:46 +0800

    feat: test homebrew install

--- brew-install.sh
+++ brew-install.sh
@@ -2,5 +2,28 @@
 
 echo $POSIXLY_CORRECT
 
-unset POSIXLY_CORRECT
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if ! command -v brew &> /dev/null; then
+  echo "Installing Homebrew..."
+  export NONINTERACTIVE=1
+  export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
+  export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
+  export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
+
+  unset POSIXLY_CORRECT
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  #echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+  #eval "$(/opt/homebrew/bin/brew shellenv)"
+  brew --verstion
+
+  if command -v brew &> /dev/null; then
+    echo "homebrew is installed."
+  else
+    echo "homebrew installation failed, please install it manually."
+    exit 1
+  fi
+else
+  echo "homebrew is installed."
+fi
+
+exit 1
--- install.sh
+++ install.sh
@@ -25,30 +25,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 chsh -s /bin/zsh
 
 # brew
-if ! command -v brew &> /dev/null; then
-  echo "Installing Homebrew..."
-  export NONINTERACTIVE=1
-  export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
-  export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
-  export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
-
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-  #echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
-  #eval "$(/opt/homebrew/bin/brew shellenv)"
-  brew --verstion
-
-  if command -v brew &> /dev/null; then
-    echo "homebrew is installed."
-  else
-    echo "homebrew installation failed, please install it manually."
-    exit 1
-  fi
-else
-  echo "homebrew is installed."
-fi
-
-exit 1
+sh ./brew-isntall.sh
 
 # brew install
 brew install git
