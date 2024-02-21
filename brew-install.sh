2c4f815f5551f714894c37d4dfb2fc7786c906f4
twitch.liu <twitch.liu@huolala.cn>
2024-02-21 11:17:55 +0800

    feat: test homebrew install

--- brew-install.sh
+++ brew-install.sh
@@ -3,4 +3,4 @@
 echo $POSIXLY_CORRECT
 
 unset POSIXLY_CORRECT
-sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
