1499b987253548988ca7fa37e5dabc32a3b51499
twitch.liu <twitch.liu@huolala.cn>
2024-02-21 10:46:06 +0800

    feat: test homebrew install

--- install.sh
+++ install.sh
@@ -25,17 +25,27 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 chsh -s /bin/zsh
 
 # brew
-echo "Installing Homebrew..."
-export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
-export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
-export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
+if ! command -v brew &> /dev/null; then
+  echo "Installing Homebrew..."
+  export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
+  export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
+  export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-#echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
-#eval "$(/opt/homebrew/bin/brew shellenv)"
+  #echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+  #eval "$(/opt/homebrew/bin/brew shellenv)"
+  brew --verstion
 
-brew --verstion
+  if command -v brew &> /dev/null; then
+    echo "homebrew is installed."
+  else
+    echo "homebrew installation failed, please install it manually."
+    exit 1
+  fi
+else
+  echo "homebrew is installed."
+fi
 
 exit 1
 
